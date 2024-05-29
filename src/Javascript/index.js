aeef03c28735671be7013f665a192c802749a18c
NikaelEnzo <enzo.marthins@gmail.com>
2024-05-29 14:19:13 -0400

    add imports to index.js

--- src/Javascript/index.js
+++ src/Javascript/index.js
@@ -1,3 +1,7 @@
+import EnemyController from "./EnemyController";
+import BulletController from "./BulletController";
+import Player from "./Player";
+
 const canvas = document.createElement('game');
 const ctx = canvas.getContext('2d'); // 2d context
 
@@ -5,4 +9,4 @@ canvas.width = 600;
 canvas.height = 600;
 
 const background = new image();
-background.src = "./src\assets\images\space.png"
+background.src = "./src/assets/images/space.png"
